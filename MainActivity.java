f4a4bf8e42b2798fefe82c3625730eefb756b60d
skynever <96180214+skynever@users.noreply.github.com>
2022-04-11 20:07:27 +0900

    로그인 회원가입 프로그램

--- MainActivity.java
+++ MainActivity.java
@@ -1,56 +1,29 @@
-package com.example.mp34;
+package com.example.database;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.media.MediaPlayer;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button pl_a, st_a;
-    View.OnClickListener cl;
-    MediaPlayer mp;
+
+    private TextView tv_id, tv_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        pl_a = (Button) findViewById(R.id.play_audio);
-        st_a = (Button) findViewById(R.id.stop_audio);
 
-        cl = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                switch (view.getId()) {
-                    case R.id.play_audio:
-                        if (mp == null) {
-                            try{
-                                mp=new MediaPlayer();
-                                mp.setDataSource("/data/user/0/com.example.mp34/lobe.mp3");
-                                mp.setDataSource("/storage/emulated/0/android/dat/com.example.mp34/file/mysong.mp3");
-                                mp.prepare();
-                                mp.start();
-                            }catch (Exception e){
-                                Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
-                            }
-                            mp = MediaPlayer.create(getApplicationContext(), R.raw.kal);
-                            mp.start();
-                        }
-                            break;
-                            case R.id.stop_audio:
-                                if (mp != null) {
-                                    mp.stop();
-                                    mp.release();
-                                    mp = null;
-                                }
-                                    break;
-                                }
+        tv_id = findViewById(R.id.tv_id);
+        tv_pass = findViewById(R.id.tv_pass);
 
-                        }
-                };
-                pl_a.setOnClickListener(cl);
-                st_a.setOnClickListener(cl);
-            };
-        }
+
+        Intent intent = getIntent();
+        String userID = intent.getStringExtra("userID");
+        String userPass = intent.getStringExtra("userPass");
+
+        tv_id.setText(userID);
+        tv_pass.setText(userPass);
+
+    }
+}
